fce85874508b6cb7aed0856c58f33b52fe6b5695
blakemerryman <blake.merryman@gmail.com>
2013-10-26 09:28:42 -0500

    Working on public interface:  * Adding public properties.  * Adding public methods.

--- KBMLinearSystem/KBMLinearSystem.h
+++ KBMLinearSystem/KBMLinearSystem.h
@@ -5,13 +5,24 @@
 //  Created by Blake Merryman on 10/26/13.
 //  Copyright (c) 2013 Blake Merryman. All rights reserved.
 //
-// The purpose of this framework is to provide a simple but
-// powerful Cocoa framework for working with linear systems.
+//  The purpose of this framework is to provide a simple but
+//  powerful Cocoa framework for working with linear systems.
 //
-
 
 #import <Foundation/Foundation.h>
 
 @interface KBMLinearSystem : NSObject
+
+#pragma mark - Public Properties:
+@property(nonatomic)NSMutableArray* array1D;    // Generic, mutable one dimensional array.
+@property(nonatomic)NSMutableArray* array2D;    // Generic, mutable two dimensional array.
+@property(nonatomic)NSUInteger* widthValue;     // Width value for arrays. Must be positive.
+@property(nonatomic)NSUInteger* heightValue;    // Height value for arrays. Must be positive.
+@property(nonatomic)NSNumber* zeroThreshold;    // If |someValue| < zeroThreshold, someValue will be considered zero.
+@property(nonatomic)NSUInteger* accuracy;       // Accuracy for x-digit arithmetic.
+
+#pragma mark - Public Methods:
+-(id)init;
+-(void)scaleVectorByFactor:(NSNumber*)factor;
 
 @end
--- KBMLinearSystem/KBMLinearSystem.m
+++ KBMLinearSystem/KBMLinearSystem.m
@@ -5,12 +5,30 @@
 //  Created by Blake Merryman on 10/26/13.
 //  Copyright (c) 2013 Blake Merryman. All rights reserved.
 //
-// The purpose of this framework is to provide a simple but
-// powerful Cocoa framework for working with linear systems.
+//  The purpose of this framework is to provide a simple but
+//  powerful Cocoa framework for working with linear systems.
 //
 
 #import "KBMLinearSystem.h"
 
+
+#pragma mark - Private Interface:
+@interface KBMLinearSystem (Private)
+
+#pragma mark Private Properties:
+@property(nonatomic)NSString* privatePropertyExample;
+
+#pragma mark Private Methods:
+-(void)privateMethodExample;
+
+@end
+
+
 @implementation KBMLinearSystem
+#pragma mark - Public Method Implementations:
+
+
+#pragma mark - Private Method Implementations:
+
 
 @end
